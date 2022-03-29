7e2be4ccd26b2f12b3797e9b531171055da1d781
swyang50066 <gog500@naver.com>
2022-03-29 19:11:32 +0900

    add results

--- source/solver.py
+++ source/solver.py
@@ -3,12 +3,12 @@ from force import drdt, dvdt
 
 def integrate_Euler_step(r, v, h):
     """Euler method"""
-
     return (r + h*drdt(r, v), v + h*dvdt(r, v))
 
 
 def integrate_EulerCromer_step(r, v, h):
     """Euler-Cramer method"""
+    # Integrate 
     v_half = v + 0.5*h*dvdt(r, v)
     r_tot = r + h*drdt(r, v_half)
     v_tot = r + 0.5*h*dvdt(r_tot, v_half)
@@ -18,20 +18,22 @@ def integrate_EulerCromer_step(r, v, h):
 
 def integrate_RungeKutta4th_step(r, v, h):
     """4th order of Runge-Kutta method"""
-    kr1 = drdt(r, v)
-    kv1 = dvdt(r, v)
+    # Runge-Kutta coefficients
+    r_k1 = drdt(r, v)
+    v_k1 = dvdt(r, v)
 
-    kr2 = drdt(r + 0.5*h*kr1, v + 0.5*h*kv1)
-    kv2 = dvdt(r + 0.5*h*kr1, v + 0.5*h*kv1)
+    r_k2 = drdt(r + 0.5*h*r_k1, v + 0.5*h*v_k1)
+    v_k2 = dvdt(r + 0.5*h*r_k1, v + 0.5*h*v_k1)
 
-    kr3 = drdt(r + 0.5*h*kr2, v + 0.5*h*kv2)
-    kv3 = dvdt(r + 0.5*h*kr2, v + 0.5*h*kv2)
+    r_k3 = drdt(r + 0.5*h*r_k2, v + 0.5*h*v_k2)
+    v_k3 = dvdt(r + 0.5*h*r_k2, v + 0.5*h*v_k2)
 
-    kr4 = drdt(r + h*kr3, v + h*kv3)
-    kv4 = dvdt(r + h*kr3, v + h*kv3)
+    r_k4 = drdt(r + h*r_k3, v + h*v_k3)
+    v_k4 = dvdt(r + h*r_k3, v + h*v_k3)
 
-    r_tot = r + h*(kr1 + 2.*kr2 + 2.*kr3 + kr4)/6.
-    v_tot = v + h*(kv1 + 2.*kv2 + 2.*kv3 + kv4)/6.
+    # Updates
+    r_tot = r + h*(r_k1 + 2.*r_k2 + 2.*r_k3 + r_k4)/6.
+    v_tot = v + h*(v_k1 + 2.*v_k2 + 2.*v_k3 + v_k4)/6.
 
     return (r_tot, v_tot)
 
