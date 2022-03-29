ebacdc13b5974ea4331e93cce35a352635300305
swyang50066 <gog500@naver.com>
2022-03-29 14:51:23 +0900

    fix typos

--- source/force.py
+++ source/force.py
@@ -5,7 +5,7 @@ from allvar import *
 
 def distance(r1, r2):
     """Return Euclidean distance between positions"""
-    return np.sqrt((np.array(r1) - np.array(r2))**2.)
+    return np.sqrt(np.sum((np.array(r1) - np.array(r2))**2.))
 
 
 def drdt(r, v):
@@ -15,9 +15,10 @@ def drdt(r, v):
     :param v: shape: (vx_earth, vy_earth, vx_jupiter, vy_jupiter) 
     :return: velocities
     """
-    return v
+    return np.array(v)
 
-def dvdt(r, v, eps=1.e-32):
+
+def dvdt(r, v, eps=1.e-20):
     """Return position derivative
 
     Central star have fixed position at (0, 0)
@@ -26,52 +27,42 @@ def dvdt(r, v, eps=1.e-32):
     :param v: shape: (vx_earth, vy_earth, vx_jupiter, vy_jupiter)
     :return: accelerations
     """
-    # Normalization factors
-    SOLAR_EARTH_NORM = G*SOLAR_MASS*EARTH_MASS*YEAR_TO_SEC**2./AU_TO_CM**3.
-    SOLAR_JUPITER_NORM = G*SOLAR_MASS*JUPITER_MASS*YEAR_TO_SEC**2./AU_TO_CM**3.
-    EARTH_JUPITER_NORM = G*EARTH_MASS*JUPITER_MASS*YEAR_TO_SEC**2./AU_TO_CM**3.
-
+    r, v = np.array(r), np.array(v)
+    
     # Geometric measurements
     r_se, r_sj, r_ej = r[:2], r[2:], r[2:] - r[:2]
     dist_se = distance((0, 0), r_se)
     dist_sj = distance((0, 0), r_sj)
     dist_ej = distance(r_se, r_sj)
 
-    theta_se = np.math.athan(np.abs(r_se[1])/(np.abs(r_se[0]) + eps))
-    theta_sj = np.math.athan(np.abs(r_sj[1])/(np.abs(r_sj[0]) + eps))
-    theta_ej = np.math.athan(np.abs(r_ej[1])/(np.abs(r_ej[0]) + eps))
+    theta_se = np.math.atan(np.abs(r_se[1])/(np.abs(r_se[0]) + eps))
+    theta_sj = np.math.atan(np.abs(r_sj[1])/(np.abs(r_sj[0]) + eps))
+    theta_ej = np.math.atan(np.abs(r_ej[1])/(np.abs(r_ej[0]) + eps))
     
-    # Unit force functionals 
-    f_se = (
-        -np.sign(r_se)
-        * np.array(
-            [
-                np.cos(theta_se)/(dist_se + eps)**2.
-                np.sin(theta_se)/(dist_se + eps)**2.
-            ]
-        )
+    # Unit force functionals
+    const_se = GG*(EARTH_MASS/SOLAR_MASS)
+    f_se = -np.sign(r_se)*const_se*np.array(
+        [      
+            np.cos(theta_se)/(dist_se + eps)**2.,
+            np.sin(theta_se)/(dist_se + eps)**2.
+        ]
     )
-    f_sj = (
-        -np.sign(r_sj)
-        * np.array(
-            [
-                np.cos(theta_sj)/(dist_sj + eps)**2.
-                np.sin(theta_sj)/(dist_sj + eps)**2.
-            ]
-        )
+    const_sj = GG*(JUPITER_MASS/SOLAR_MASS)
+    f_sj = -np.sign(r_sj)*const_sj*np.array(
+        [
+            np.cos(theta_sj)/(dist_sj + eps)**2.,
+            np.sin(theta_sj)/(dist_sj + eps)**2.
+        ]
     )
-    f_ej = (
-        -np.sign(r_ej)
-        * np.array(
-            [
-                np.cos(theta_ej)/(dist_ej + eps)**2.
-                np.sin(theta_ej)/(dist_ej + eps)**2.
-            ]
-        )
+    const_ej = GG*(EARTH_MASS*JUPITER_MASS/SOLAR_MASS**2.)
+    f_ej = -np.sign(r_ej)*const_ej*np.array(
+        [
+            np.cos(theta_ej)/(dist_ej + eps)**2.,
+            np.sin(theta_ej)/(dist_ej + eps)**2.
+        ]
     )
-
-    # Accelearations
-    a_e = SOLAR_EARTH_NORM*f_se - EARTH_JUPITER_NORM*f_ej 
-    a_j = SOLAR_JUPITER_NORM*f_sj + EARTH_JUPITER_NORMf_ej
-
-    return np.hstack([a_e, a_j])
+    
+    return np.hstack([
+        (f_se - f_ej)/(EARTH_MASS/SOLAR_MASS),
+        (f_sj + f_ej)/(JUPITER_MASS/SOLAR_MASS),
+    ])
--- source/solver.py
+++ source/solver.py
@@ -3,10 +3,11 @@ from force import drdt, dvdt
 
 def integrate_Euler_step(r, v, h):
     """Euler method"""
+
     return (r + h*drdt(r, v), v + h*dvdt(r, v))
 
 
-def integrate_EuleCromer_step(r, v, h):
+def integrate_EulerCromer_step(r, v, h):
     """Euler-Cramer method"""
     v_half = v + 0.5*h*dvdt(r, v)
     r_tot = r + h*drdt(r, v_half)
@@ -24,7 +25,7 @@ def integrate_RungeKutta4th_step(r, v, h):
     kv2 = dvdt(r + 0.5*h*kr1, v + 0.5*h*kv1)
 
     kr3 = drdt(r + 0.5*h*kr2, v + 0.5*h*kv2)
-    kr3 = dvdt(r + 0.5*h*kr2, v + 0.5*h*hv2)
+    kv3 = dvdt(r + 0.5*h*kr2, v + 0.5*h*kv2)
 
     kr4 = drdt(r + h*kr3, v + h*kv3)
     kv4 = dvdt(r + h*kr3, v + h*kv3)
